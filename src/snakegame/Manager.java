301c19d1ccb5321631458be38a1bc74bd29c0fcc
dolphin235 <dolphin222@daum.net>
2022-02-19 15:47:42 +0900

    add snake body

--- src/snakegame/Manager.java
+++ src/snakegame/Manager.java
@@ -3,7 +3,34 @@ import java.util.Scanner;
 
 public class Manager {
     public static void main(String[] args) {
+    	
+    	while(true) {
+    		run_game();
+    		if(!check_continue()) {
+    			break;
+    		}
+    	}
+    	System.out.println("FINISH !");
+    }
+    
+    public static boolean check_continue() {
+    	System.out.println("GAME OVER");
+    	System.out.println("Do you want to start snake game?");
+    	System.out.println("1: start, other: stop");
+    	
+        Scanner scanner = new Scanner(System.in);
+        char input;
+        input = scanner.nextLine().charAt(0);
  
+        if (input == '1') {
+        	return true;
+        }
+        else {
+        	return false;
+        }
+    }
+ 
+    public static void run_game() {
         final int BOARD_WIDTH = 20; 
         final int BOARD_HEIGHT = 10; 
         final int START_X = BOARD_WIDTH / 2;
@@ -50,16 +77,34 @@ public class Manager {
             }
             if (check_eat(snake, apple)) {
             	apple.addRandomApple(board, apple);
+            	snake.eat_apple();
             	score += 10;
+            }
+            else {
+            	if (check_conflict_wall(snake, board)) {
+            		return;
+            	}
             }
         }
     }
+    
     public static boolean check_eat(SnakeElement snake, AppleElement apple) {
-    if ((snake.getX() == apple.getX()) && (snake.getY() == apple.getY())) {
-    	return true;
-		}
-    else {
-    	return false;
+    	if ((snake.getX() == apple.getX()) && (snake.getY() == apple.getY())) {
+    		return true;
+    	}
+	    else {
+	    	return false;
+	    }
+    }
+    
+    public static boolean check_conflict_wall(SnakeElement snake, Board board) {
+    	if ((snake.getX() == 0) || (snake.getX() == (board.getBoardWidth() - 1)) || 
+    			(snake.getY() == 0) || (snake.getY() == (board.getBoardHeight() -1))) {
+    		return true;
+    	}
+    	else {
+    		return false;
     	}
     }
+    
 }
--- src/snakegame/SnakeElement.java
+++ src/snakegame/SnakeElement.java
@@ -1,34 +1,150 @@
 package snakegame;
 
 public class SnakeElement extends BoardComponent {
-	 
+	
+	private int body_len;
+	private int body_x[];
+	private int body_y[];
+	
+	private char head_symbol;
+	
+	private boolean eat;
+	
+	@Override
+	public void setX(int newLocation) {
+		this.body_x[0] = newLocation;
+	}
+	
+	@Override
+	public void setY(int newLocation) {
+		this.body_y[0] = newLocation;
+	}
+	
+	@Override
+	public int getX() {
+		return this.body_x[0];
+	}
+	
+	@Override
+	public int getY() {
+		return this.body_y[0];
+	}
+
     public SnakeElement(char symbol, int xStartingLocation, int yStartingLocation) {
-        setIcon(symbol);
+        this.body_len = 1;
+        this.body_x = new int[50];
+        this.body_y = new int[50];
+        
+        this.head_symbol = 'o';
+        this.eat = false;
+    	setIcon(symbol);
         setX(xStartingLocation);
         setY(yStartingLocation);
+       
     }
-     
+    
+    public void eat_apple() {
+    	this.body_len += 1;
+    	this.eat = true;
+    }
+
     public void moveLeft(Board screen, SnakeElement snake) {
-        snake.setX(getX() - 1);
-        screen.setObjectOnLocation(snake, snake.getX(), snake.getY());
-        screen.ClearScreenLocation(snake.getX() + 1, snake.getY());
+    	
+    	int[] new_location_x = new int[snake.body_len];
+    	int[] new_location_y = new int[snake.body_len];
+    	
+        new_location_x[0] = snake.getX() - 1;
+        new_location_y[0] = snake.getY();
+        screen.setObjectOnLocation(snake.head_symbol, new_location_x[0], new_location_y[0]);
+        
+        for(int i=1;i<snake.body_len;i++) {
+        	new_location_x[i] = snake.body_x[i-1];
+        	new_location_y[i] = snake.body_y[i-1];
+        	screen.setObjectOnLocation(snake, new_location_x[i], new_location_y[i]);
+        }
+        
+        if (this.eat == true) {
+        	this.eat = false;
+        }
+        else {
+        	screen.ClearScreenLocation(snake.body_x[snake.body_len-1], snake.body_y[snake.body_len-1]);
+        }
+        this.body_x = new_location_x;
+        this.body_y = new_location_y;
     }
      
     public void moveRight(Board screen, SnakeElement snake) {
-    	snake.setX(getX() + 1);
-    	screen.setObjectOnLocation(snake, snake.getX(), snake.getY());
-    	screen.ClearScreenLocation(snake.getX() - 1, snake.getY());
+    	
+    	int[] new_location_x = new int[snake.body_len];
+    	int[] new_location_y = new int[snake.body_len];
+    	
+    	new_location_x[0] = snake.getX() + 1;
+    	new_location_y[0] = snake.getY();
+    	screen.setObjectOnLocation(snake.head_symbol, new_location_x[0], new_location_y[0]);
+    	
+        for(int i=1;i<snake.body_len;i++) {
+        	new_location_x[i] = snake.body_x[i-1];
+        	new_location_y[i] = snake.body_y[i-1];
+        	screen.setObjectOnLocation(snake, new_location_x[i], new_location_y[i]);
+        }
+    	
+    	if (this.eat == true) {
+    		this.eat = false;
+    	}
+    	else {
+    		screen.ClearScreenLocation(snake.body_x[snake.body_len-1], snake.body_y[snake.body_len-1]);
+    	}
+        this.body_x = new_location_x;
+        this.body_y = new_location_y;
     }
      
     public void moveUp(Board screen, SnakeElement snake) {
-    	snake.setY(getY() - 1);
-        screen.setObjectOnLocation(snake, snake.getX(), snake.getY());
-        screen.ClearScreenLocation(snake.getX(), snake.getY() + 1);
+    	
+    	int[] new_location_x = new int[snake.body_len];
+    	int[] new_location_y = new int[snake.body_len];
+    	
+    	new_location_x[0] = snake.getX();
+    	new_location_y[0] = snake.getY() - 1;
+    	screen.setObjectOnLocation(snake.head_symbol, new_location_x[0], new_location_y[0]);
+    	
+        for(int i=1;i<snake.body_len;i++) {
+        	new_location_x[i] = snake.body_x[i-1];
+        	new_location_y[i] = snake.body_y[i-1];
+        	screen.setObjectOnLocation(snake, new_location_x[i], new_location_y[i]);
+        }
+        
+        if (this.eat == true) {
+        	this.eat = false;
+        }
+        else {
+        	screen.ClearScreenLocation(snake.body_x[snake.body_len-1], snake.body_y[snake.body_len-1]);
+        }
+        this.body_x = new_location_x;
+        this.body_y = new_location_y;
     }
      
     public void moveDown(Board screen, SnakeElement snake) {
-    	snake.setY(getY() + 1);
-        screen.setObjectOnLocation(snake, snake.getX(), snake.getY());
-        screen.ClearScreenLocation(snake.getX(), snake.getY() - 1);
+    	
+    	int[] new_location_x = new int[snake.body_len];
+    	int[] new_location_y = new int[snake.body_len];
+    	
+    	new_location_x[0] = snake.getX();
+    	new_location_y[0] = snake.getY() + 1;
+    	screen.setObjectOnLocation(snake.head_symbol, new_location_x[0], new_location_y[0]);
+    	
+        for(int i=1;i<snake.body_len;i++) {
+        	new_location_x[i] = snake.body_x[i-1];
+        	new_location_y[i] = snake.body_y[i-1];
+        	screen.setObjectOnLocation(snake, new_location_x[i], new_location_y[i]);
+        }
+        
+        if (this.eat == true) {
+        	this.eat = false;
+        }
+        else {
+        	screen.ClearScreenLocation(snake.body_x[snake.body_len-1], snake.body_y[snake.body_len-1]);
+        }
+        this.body_x = new_location_x;
+        this.body_y = new_location_y;
     }
 }
